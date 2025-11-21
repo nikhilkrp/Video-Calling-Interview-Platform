1167e5ed07d66411fa7a93567d11551bfe3f19d4
Nikhil Kumar Prajapati <np1626978@gmmail.com>
2025-11-21 18:54:24 +0530

    setup backend codebase

---
+++ backend/src/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import { ENV } from './lib/env.js';
+
+
+
+
+
+const app = express();
+
+
+app.use(express.json());
+
+
+
+app.get('/', (req, res) => {
+    res.send('Server api is up and running');
+})
+
+app.listen(ENV.PORT, () => {
+    console.log(`Server is running on port ${ENV.PORT}`);
+})
