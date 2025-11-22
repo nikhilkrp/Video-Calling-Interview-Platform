b50ccead121e266756c5e964240bb998b9ca320f
Nikhil Kumar Prajapati <np1626978@gmmail.com>
2025-11-22 11:14:17 +0530

    sync auth and db

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { ENV } from './lib/env.js';
 import connectDB from './lib/db.js';
+import cors from 'cors' 
+import { functions, inngest } from './lib/inngest.js';
 
 
 
@@ -10,8 +12,9 @@ const app = express();
 
 
 app.use(express.json());
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
 
-
+app.use('/api/inngest',serve({client:inngest,functions}))
 
 app.get('/', (req, res) => {
     res.send('Server api is up and running');
