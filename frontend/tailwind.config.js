93935e057163e89359df91866b34a45a0020f490
Nikhil Kumar Prajapati <np1626978@gmmail.com>
2025-12-02 11:08:20 +0530

    HomePAge-setup

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,5 @@
+import themes from 'daisyui/theme/object';
+
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
@@ -9,6 +11,11 @@ export default {
   },
   plugins: [
     require("daisyui"),
+    
   ],
+  daisyui:{
+    themes:true,
+  }
+
 }
 
